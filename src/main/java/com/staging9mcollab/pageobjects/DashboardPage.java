e32384c1b3524b78a4d8b2c4c1b48cfaf14f3752
Zoran Dragovic <kizdajica@gmail.com>
2019-01-13 19:44:17 +0100

    Dashboard page created

---
+++ src/main/java/com/staging9mcollab/pageobjects/DashboardPage.java
@@ -0,0 +1,21 @@
+package com.staging9mcollab.pageobjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+/**
+ * This class models the DashboardPage Page and its objects
+ *
+ * @author Zoran Dragovic
+ */
+public class DashboardPage {
+    @FindBy(how = How.XPATH, using = "/html/body/div/div[4]/div/div[1]/div/div[1]/div[2]/div/button[1]")
+    private WebElement createAGroup;
+
+    public void clickCreateGroup() {
+        createAGroup.click();
+    }
+
+
+}

12b08e24ae17699731420a82873680d48e565ec2
Zoran Dragovic <kizdajica@gmail.com>
2019-01-13 17:09:05 +0100

    BaseTest class created. chromedriver.exe added to the project. Properties file created.

--- src/main/java/com/staging9mcollab/baseclasses/BaseTest.java
+++ src/main/java/com/staging9mcollab/baseclasses/BaseTest.java
@@ -1,4 +1,30 @@
 package com.staging9mcollab.baseclasses;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * This is the Base Test class that manipulates with the driver
+ *
+ * @author Zoran Dragovic
+ */
 public class BaseTest {
+    private WebDriver driver;
+
+    /**
+     * This method initializes the driver and navigates to AUT
+     */
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromeDriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://automationtesting.staging-9mcollab.com");
+    }
+
+    /**
+     * This method closes the driver
+     */
+    public void tearDown() {
+        driver.quit();
+    }
 }

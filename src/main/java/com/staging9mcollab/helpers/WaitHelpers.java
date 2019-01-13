58a8645f834b07d589b13a81830146d328b5f784
Zoran Dragovic <kizdajica@gmail.com>
2019-01-13 18:58:33 +0100

    WaitHelpers class added.

---
+++ src/main/java/com/staging9mcollab/helpers/WaitHelpers.java
@@ -0,0 +1,50 @@
+package com.staging9mcollab.helpers;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WaitHelpers {
+
+    public static Boolean waitUntilElementIsPresent(WebDriver driver, By locator, long seconds) {
+        WebDriverWait wait = new WebDriverWait(driver, seconds);
+        try {
+            wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static Boolean waitUntilElementIsVisible(WebDriver driver, By locator, long seconds) {
+        WebDriverWait wait = new WebDriverWait(driver, seconds);
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static Boolean waitUntilElementIsClickable(WebDriver driver, By locator, long seconds) {
+        WebDriverWait wait = new WebDriverWait(driver, seconds);
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(locator));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
+    public static Boolean waitUntilTitleIs(WebDriver driver, String title, long seconds) {
+        WebDriverWait wait = new WebDriverWait(driver, seconds);
+        try {
+            wait.until(ExpectedConditions.titleIs(title));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}

fccff3bc98207250d588452b9637838938768dda
Zoran Dragovic <kizdajica@gmail.com>
2019-01-13 16:47:25 +0100

    Login page object modeled.

--- src/main/java/com/staging9mcollab/pageobjects/LoginPage.java
+++ src/main/java/com/staging9mcollab/pageobjects/LoginPage.java
@@ -1,4 +1,27 @@
 package com.staging9mcollab.pageobjects;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+/**
+ * This class models the Login Page and its objects
+ *
+ * @author Zoran Dragovic
+ */
 public class LoginPage {
+    @FindBy(how = How.ID, using = "username")
+    public WebElement email;
+
+    @FindBy(how = How.ID, using = "password")
+    public WebElement password;
+
+    @FindBy(how = How.ID, using = "rememberMe")
+    public WebElement rememberMe;
+
+    @FindBy(how = How.LINK_TEXT, using = "Forgot Password?")
+    public WebElement forgotPassword;
+
+    @FindBy(how = How.LINK_TEXT, using = "kc-login")
+    public WebElement logIn;
 }
